aa948817e77a26e157ae28e91db5afa399d5c1f4
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-01-13 18:04:51 -0300

    Fix case

--- src/websocket.js
+++ src/websocket.js
@@ -1,11 +1,11 @@
 import Socket from 'socket.io';
 import redis from 'redis';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import redis_mock from 'redis-mock';
+import redisMock from 'redis-mock';
 
 let { createClient } = redis;
 if (process.env.NODE_ENV === 'test') {
-  createClient = redis_mock.createClient;
+  createClient = redisMock.createClient;
 }
 
 const client = createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
@@ -22,14 +22,14 @@ export function setupWebSocket(server) {
 }
 
 export async function findConnection(id) {
-  const socket_id = await new Promise(resolve => {
-    client.get(id.toString(), (err, reply) => {
+  const socketId = await new Promise(resolve => {
+    client.get(id.toString(), (_, reply) => {
       resolve(reply);
     });
   });
 
-  if (typeof socket_id === 'string') {
-    return socket_id;
+  if (typeof socketId === 'string') {
+    return socketId;
   }
   return null;
 }
