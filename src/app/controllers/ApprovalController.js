786a7d34a88548bb9c84d7c6ee1c9c1bb1498d58
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-01-14 18:32:59 -0300

    Update controller cases and call methods names

--- src/app/controllers/ApprovalController.js
+++ src/app/controllers/ApprovalController.js
@@ -1,21 +1,26 @@
 import EmitBooking from '../services/EmitBooking';
 import ApproveBooking from '../services/ApproveBooking';
 
+const approveBooking = new ApproveBooking();
+const emitBooking = new EmitBooking();
+
 class ApprovalController {
   async store(req, res) {
     const { booking_id } = req.params;
     const { user_id: user } = req;
 
-    const booking = await ApproveBooking.run({ booking_id, user });
+    const booking = await approveBooking.execute({ booking_id, user });
 
-    await EmitBooking.run({
-      user_id: booking.user,
-      booking,
-      event: 'booking_response',
-    });
+    if (user !== booking.user.toString()) {
+      await emitBooking.execute({
+        user_id: booking.user,
+        booking,
+        event: 'booking_response',
+      });
+    }
 
     return res.json(booking);
   }
 }
 
-export default new ApprovalController();
+export default ApprovalController;
