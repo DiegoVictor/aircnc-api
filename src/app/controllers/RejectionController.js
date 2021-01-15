215ff88170cb2e0da59250b722e0a83bd869bfd3
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-01-15 14:29:15 -0300

    Update booking reject controller

--- src/app/controllers/RejectionController.js
+++ src/app/controllers/RejectionController.js
@@ -1,21 +1,26 @@
 import EmitBooking from '../services/EmitBooking';
 import RejectBooking from '../services/RejectBooking';
 
+const emitBooking = new EmitBooking();
+const rejectBooking = new RejectBooking();
+
 class RejectionController {
   async store(req, res) {
     const { user_id: user } = req;
     const { booking_id } = req.params;
 
-    const booking = await RejectBooking.run({ booking_id, user });
+    const booking = await rejectBooking.execute({ booking_id, user });
 
-    await EmitBooking.run({
-      user_id: booking.user,
-      booking,
-      event: 'booking_response',
-    });
+    if (user !== booking.user.toString()) {
+      await emitBooking.execute({
+        user_id: booking.user,
+        booking,
+        event: 'booking_response',
+      });
+    }
 
     return res.json(booking);
   }
 }
 
-export default new RejectionController();
+export default RejectionController;
