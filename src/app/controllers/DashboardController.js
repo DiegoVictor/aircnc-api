438cf933e6f3d0e3f24b5bb958dc96f6f82cf291
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-01-16 13:31:18 -0300

    Return spots' url

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -2,12 +2,19 @@ import Spot from '../models/Spot';
 
 class DashboardController {
   async index(req, res) {
-    const { user_id: user } = req;
+    const { user_id: user, hostUrl } = req;
+
     const spots = await Spot.find({ user });
+
     const count = await Spot.countDocuments({ user });
     res.header('X-Total-Count', count);
 
-    return res.json(spots);
+    return res.json(
+      spots.map(spot => ({
+        ...spot.toJSON(),
+        url: `${hostUrl}/v1/spots/${spot._id}`,
+      }))
+    );
   }
 }
 
