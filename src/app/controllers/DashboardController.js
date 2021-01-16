4e4fc92e0e70bdf8bded9134887243674453647d
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-01-16 13:30:53 -0300

    Set items amout header

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -4,6 +4,9 @@ class DashboardController {
   async index(req, res) {
     const { user_id: user } = req;
     const spots = await Spot.find({ user });
+    const count = await Spot.countDocuments({ user });
+    res.header('X-Total-Count', count);
+
     return res.json(spots);
   }
 }
