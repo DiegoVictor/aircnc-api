27190af08994c1c9c0d6b811d0a6e686cd51246b
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-01-16 13:31:40 -0300

    Return bookings' spots url

--- src/app/controllers/PendingController.js
+++ src/app/controllers/PendingController.js
@@ -3,7 +3,7 @@ import Spot from '../models/Spot';
 
 class PendingController {
   async index(req, res) {
-    const { user_id: user } = req;
+    const { user_id: user, hostUrl } = req;
     const spots = await Spot.find({ user });
     const bookings = await Booking.find({
       approved: { $exists: false },
@@ -13,7 +13,15 @@ class PendingController {
       .populate('user')
       .populate('spot');
 
-    return res.json(bookings);
+    return res.json(
+      bookings.map(booking => ({
+        ...booking.toJSON(),
+        spot: {
+          ...booking.spot.toJSON(),
+          url: `${hostUrl}/v1/spots/${booking.spot._id}`,
+        },
+      }))
+    );
   }
 }
 
